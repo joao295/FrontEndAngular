31a20c974c7f5273bc2eded3c611320c642a0a0f
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-24 09:30:07 -0300

    [Add consumo-part1]

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,10 @@
+import { Postagem } from "./postagem"
+
+
+export class Tema {
+   
+    public id: number
+    public descricao: string
+    public postagem:Postagem[]
+
+}
---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+export class UserLogin{
+
+    public id: number
+    public nome: string
+    public foto:string
+    public usuario: string
+    public senha:string
+    public tipo :string
+    public token:string
+}
